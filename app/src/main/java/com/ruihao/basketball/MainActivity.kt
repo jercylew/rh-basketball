6aea411551cac78e450a2f75cb10a77848cf714e
Yuming Liu <jercy_lew@yahoo.com>
2023-08-27 23:31:30 +0800

    Entering fullscreen mode when launched

--- app/src/main/java/com/ruihao/basketball/MainActivity.kt
+++ app/src/main/java/com/ruihao/basketball/MainActivity.kt
@@ -2,7 +2,13 @@ package com.ruihao.basketball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.TextView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.ruihao.basketball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +16,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        actionBar?.hide()
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        setContentView(binding.root)
+        setContentView(R.layout.test_layout)
 
         // Example of a call to a native method
 //        binding.sampleText.text = stringFromJNI()
-        binding.sampleText.text = doFaceRecognition("/path/images/yuming.jpg");
+        binding.sampleText.text = doFaceRecognition("/path/images/yuming.jpg")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Do the cleaning work
+        var num: Int = 10
     }
 
     /**

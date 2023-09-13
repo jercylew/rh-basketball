9e8acad75ef6be577e3d7b4f2d70488a248391be
Yuming Liu <jercy_lew@yahoo.com>
2023-09-13 23:13:01 +0800

    Add camera preview

--- app/src/main/java/com/ruihao/basketball/MainActivity.kt
+++ app/src/main/java/com/ruihao/basketball/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.ruihao.basketball
 
+import android.Manifest
 import android.os.Bundle
+import android.os.Build
+import android.provider.MediaStore
+import android.content.ContentValues
+import android.content.pm.PackageManager
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.KeyEvent
@@ -16,6 +21,18 @@ import com.ruihao.basketball.databinding.ActivityMainBinding
 import java.io.IOException
 import java.security.InvalidParameterException
 import java.util.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import java.util.concurrent.ExecutorService
+import androidx.core.content.ContextCompat
+import androidx.camera.core.CameraSelector
+import java.text.SimpleDateFormat
 import com.chaquo.python.PyException
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
@@ -37,6 +54,33 @@ class MainActivity : AppCompatActivity() {
     private var dispQueue: DispQueueThread? = null
     private var comPort: SerialControl? = null
 
+    //Camera
+    private lateinit var viewBinding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         actionBar?.hide()
@@ -51,8 +95,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-        setContentView(R.layout.basketball_home)
+        setContentView(binding.root)
+//        setContentView(R.layout.basketball_home)
         
         if (!Python.isStarted()) {
             Python.start(AndroidPlatform(this))
@@ -109,6 +153,7 @@ class MainActivity : AppCompatActivity() {
 
         }
         mBtnReturn.setOnClickListener {
+            takePhoto()
             if (mUser == null) {
                 Toast.makeText(this@MainActivity, getString(R.string.tip_login),
                     Toast.LENGTH_LONG).show()
@@ -139,9 +184,15 @@ class MainActivity : AppCompatActivity() {
 
         dispQueue!!.start()
 
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions()
+        }
+
         // Example of a call to a native method
-//        binding.sampleText.text = stringFromJNI()
-        binding.sampleText.text = doFaceRecognition("/path/images/yuming.jpg")
+//        binding.sampleText.text = doFaceRecognition("/path/images/yuming.jpg")
     }
 
     override fun onResume() {
@@ -160,6 +211,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         comPort?.close()
         dispQueue?.interrupt()
+        cameraExecutor.shutdown()
 
         // Do the cleaning work
         var num: Int = 10
@@ -262,8 +314,95 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Camera
     private fun initCamera() {
 
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+
+    private fun captureVideo() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        imageCapture = ImageCapture.Builder().build()
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun dispRecData(comRecData: ComBean) {
@@ -378,7 +517,20 @@ class MainActivity : AppCompatActivity() {
     private external fun readModbusBit(address: Int): Int
     private external fun readModbusRegister(address: Int): Int
 
+
     companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+
         // Used to load the 'basketball' library on application startup.
         init {
             System.loadLibrary("basketball")

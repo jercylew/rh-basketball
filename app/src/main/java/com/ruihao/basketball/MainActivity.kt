60a742fcb2c62b4adce3b0a0942d0fe360d3cd05
Yuming Liu <jercy_lew@yahoo.com>
2023-12-05 11:31:22 +0800

    Cancel the dialog if the user enter the incorrect password

--- app/src/main/java/com/ruihao/basketball/MainActivity.kt
+++ app/src/main/java/com/ruihao/basketball/MainActivity.kt
@@ -394,6 +394,9 @@ class MainActivity : AppCompatActivity() {
                             }
                             loginUser(BaseColumns._ID, "00000000-0000-0000-0000-000000000000")
                         }
+                        else {
+                            dialog.cancel()
+                        }
                     })
                 .setNegativeButton(R.string.dialog_cancel,
                     DialogInterface.OnClickListener { dialog, id ->
